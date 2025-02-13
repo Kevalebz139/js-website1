f1a550271e3e195864d9052286033f4112a8085d
Kevalebz139 <kevalebz@gmail.com>
2025-02-13 10:20:16 +0530

    cart quantity update using js and saved data in cart.js

--- scripts/script.js
+++ scripts/script.js
@@ -37,5 +37,33 @@ document.querySelectorAll('.js-add-to-cart')
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
 
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+        console.log(cart);
+
   });
 });
